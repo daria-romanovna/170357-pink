9659d72a95afa62ef566eb4266b7a5d2c6de9b11
daria-romanovna <gorbunova.daria@gmail.com>
2016-04-07 17:18:29 +0700

    Сборка проекта

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,28 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var copy = require('gulp-contrib-copy');
+var clean = require('gulp-contrib-clean');
+
+gulp.task( "copy", function() {
+  gulp.src(".")
+    .pipe(copy(
+      "fonts/**/*.{woff,woff2}",
+      "img/**",
+      "js/**",
+      "*.html"
+    ))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('clean', function() {
+  gulp.src('build')
+    .pipe(clean());
+});
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
@@ -18,10 +40,26 @@ gulp.task("style", function() {
         "last 2 Firefox versions",
         "last 2 Opera versions",
         "last 2 Edge versions"
-      ]})
+      ]}),
+      mqpacker ({
+        sort: true
+      })
     ]))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
     .pipe(server.reload({stream: true}));
+});
+
+
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
+    .pipe(imagemin({
+      optimizationLevel: 3,
+      progressive: true
+    }))
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("serve", ["style"], function() {
